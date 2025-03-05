f1c1249c81ac68ad20d56ca6533da1f36a0ed09b
Paulo <paulomsmsm@gmail.com>
2025-03-05 12:53:39 -0300

    hotfix precision e scale

--- src/main/java/me/dio/dio_api_rest_project/model/Account.java
+++ src/main/java/me/dio/dio_api_rest_project/model/Account.java
@@ -20,9 +20,9 @@ public class Account {
 
     private String agency;
     
-    @Column(scale = 13, precision = 2)
+    @Column(precision = 13, scale = 2)
     private BigDecimal balance;
-    @Column(name = "additional_limit", scale = 13, precision = 2)
+    @Column(name = "additional_limit", precision = 13, scale = 2)
     private BigDecimal limit;
 
     public Long getId() {
@@ -55,7 +55,5 @@ public class Account {
     public void setLimit(BigDecimal limit) {
         this.limit = limit;
     }
-
-
 
 }
